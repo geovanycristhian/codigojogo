d708f969276fa63750d8225ad085841004dfc4e0
GeovanyCristhian <geovanycristhian30@gmail.com>
2023-11-20 21:06:54 -0300

    Create codigo.py

---
+++ codigo.py
@@ -0,0 +1,20 @@
+class Heroi:
+    def __init__(self, nome, idade, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+
+    def atacar(self):
+        if self.tipo == "mago":
+            ataque = "usou magia"
+        elif self.tipo == "guerreiro":
+            ataque = "usou espada"
+        elif self.tipo == "monge":
+            ataque = "usou artes marciais"
+        elif self.tipo == "ninja":
+            ataque = "usou shuriken"
+        else:
+            ataque = "usou um ataque indefinido"
+
+        mensagem = f"O {self.tipo} {self.nome} atacou usando {ataque}"
+        print(mensagem)
